928537726d4048072200e8cc94d58d279145582c
Chanmuzi <101971295+Chanmuzi@users.noreply.github.com>
2022-07-22 19:49:04 +0900

    Update 같은 숫자는 싫어_다른 풀이.py

--- Level 1/같은 숫자는 싫어_다른 풀이.py
+++ Level 1/같은 숫자는 싫어_다른 풀이.py
@@ -3,8 +3,8 @@ def solution(s):
     a = []
     # 입력 받은 리스트의 각 요소에 대해
     for i in s:
-        # i부터 끝까지 같은 것이 지속될 동안은 패스
+        # 정답 리스트의 맨 마지막 요소와 같다면 패스
         if a[-1:] == [i]: continue
-        # 같은 것이 없을 때는 추가
+        # 같지 않을 때는 추가
         a.append(i)
     return a
